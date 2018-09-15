d5be19b50136312e57210f47dd3fec33c584a4c9
Jalil <agoumi.jalil@gmail.com>
2018-09-15 13:18:53 +0200

    maj

--- public/error.py
+++ public/error.py
@@ -3,6 +3,8 @@ err = {
 	0x0001:{"group":"Formulaire","msg":"Un champs requis est vide"},
 	0X0002:{"group":"Formulaire","msg":"Le champs n'a pas le type INTEGER"},
 	0X0003:{"group":"Formulaire","msg":"Le champs n'a pas le type DOUBLE"},
+	0X0004:{"group":"Formulaire","msg":"Le numéro de téléphone ne contient pas assez de chiffre"},
+	0X0004:{"group":"Formulaire","msg":"Le numéro de téléphone contient trop de chiffre"},
 	0X0020:{"group":"Périphériques","msg":"Vous devez d'abord configurer la souris"},
 }
 
